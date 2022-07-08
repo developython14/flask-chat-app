ac908db5bc1c7b935c426b49f7479a513c545811
developython14 <djmel1955@gmail.com>
2022-07-08 19:27:02 +0100

    config qnd link front end pages

--- static/javascript/film.js
+++ static/javascript/film.js
@@ -1,1 +1,26 @@
-console.log("mustapha file loaded");
+const api_url = 'https://www.breakingbadapi.com/api/characters' ; 
+
+
+async function getdata() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    put(toma);
+}
+
+function put(data){
+    var sel = document.querySelector('.data')
+    for (let element of data) {
+        sel.innerHTML = sel.innerHTML+  `<div class="card" style="width: 18rem;">
+        <img src="${element.img}" class="card-img-top" alt="image not appear">
+        <div class="card-body">
+          <h5 class="card-title">${element.name}</h5>
+          <h5 class="card-title">${element.nickname}</h5>
+        </div>
+      </div>`
+    }
+}
+
+
+
+
+getdata();
