eeb8d029a21b1665ce12b24e22aab187747b383f
developython14 <djmel1955@gmail.com>
2022-07-09 21:27:05 +0100

    config qnd link front end pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,jsonify
 
 app = Flask(__name__)
 
@@ -16,6 +16,12 @@ def filter_api():
 def filter_with_ajax():
     return render_template('filter_with_ajax.html')
 
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
 
 if __name__ == '__main__' :
     app.run()
