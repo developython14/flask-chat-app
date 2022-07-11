fd27329ac4d19ca97d24426eacf4c8886c0ce607
developython14 <djmel1955@gmail.com>
2022-07-11 23:01:45 +0100

    adding the flask socket io to implement the chat systeme

--- static/javascript/filter_ajax.js
+++ static/javascript/filter_ajax.js
@@ -28,13 +28,24 @@ $(function() {
 
 var socket = io();
 socket.on('connect', function() {
-    socket.emit('my event', {data: 'I\'m mustapha!'});
-    socket.emit('messagebox', {data: 'I\'m mustapha!'});
+    $('#exampleInputEmail1').bind('click', function() {
+    let user_name = $( 'input#exampleInputEmail1' ).val()
+    socket.emit('messagebox', user_name);
+    $( 'input#exampleInputEmail1' ).val( '3awed kteb' ).focus()
+    });
+
+   
 });
 
 
 socket.on( 'my response', function( msg ) {
   console.log("raha twli response mustapha");
+  console.log( msg );
+
+})
+
+
+socket.on( 'message draw', function( msg ) {
   console.log( msg );
 
 })
