9a4694e547cb0f36dd13d39a7db115263b191e2b
developython14 <djmel1955@gmail.com>
2022-07-14 09:20:58 +0100

    create the virtualenv and prepare some package

--- static/javascript/filter_ajax.js
+++ static/javascript/filter_ajax.js
@@ -1,43 +1,48 @@
+const api_url = 'https://www.breakingbadapi.com/api/characters' ; 
+
+
+async function getdata() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    put(data);
+}
+
+function put(data){
+    var sel = document.querySelector('.data')
+    for (let element of data) {
+        sel.innerHTML = sel.innerHTML+  `<div class="card" style="width: 18rem;">
+        <img src="${element.img}" class="card-img-top" alt="image not appear">
+        <div class="card-body">
+          <h5 class="card-title">${element.name}</h5>
+          <h5 class="card-title">${element.nickname}</h5>
+        </div>
+      </div>`
+    }
+}
 
 
 
-$(function() {
-  $('a#calculate').bind('click', function() {
-    $.getJSON( '/_add_numbers', {
-      a: $('input[name="a"]').val(),
-      b: $('input[name="b"]').val()
-    }, function(data) {
-      $("#result").text(data.result);
-    });
-    return false;
-  });
-});
+async function changegrid() {
+    var sel = document.querySelector('.data');
+    const response = await fetch(api_url);
+    var data = await response.json();
+    data = data.filter(checkAdult);
+    sel.innerHTML = '';
+    for (let element of data) {
+        sel.innerHTML = sel.innerHTML+  `<div class="card" style="width: 18rem;">
+        <img src="${element.img}" class="card-img-top" alt="image not appear">
+        <div class="card-body">
+          <h5 class="card-title">${element.name}</h5>
+          <h5 class="card-title">${element.nickname}</h5>
+        </div>
+      </div>`
+    }
+}
+
+function checkAdult(car) {
+    var _filter = document.querySelector('#exampleInputEmail1').value;
+    return  car.name.toUpperCase().includes(_filter.toUpperCase()) ;
+  }
 
 
-let message_colors=document.querySelector('.colors').value ;
-
-var socket = io();
-socket.on('connect', function() {
-    $('#fliterdata').bind('click', function() {
-    let user_name = $( 'input#exampleInputEmail1' ).val()
-    socket.emit('messagebox', user_name);
-    $( 'input#exampleInputEmail1' ).val( '3awed kteb' ).focus()
-    });
-
-   
-});
-
-
-socket.on( 'my response', function( msg ) {
-  console.log("user connected suuceffly");
-  console.log( msg );
-
-})
-
-
-socket.on( 'message draw', function( msg ) {
-  let frame = document.querySelector('.list-group');
-  var li = `<li class="list-group-item" style="background-color:${message_colors} ;">${msg}</li>`;
-  frame.innerHTML = frame.innerHTML + li;
-  console.log( msg );
-})
+getdata();
