118e9e0ae60cf02386210f11f87410a5317387b6
developython14 <djmel1955@gmail.com>
2022-07-15 03:26:07 +0100

    work on chat systeme

--- static/javascript/chat.js
+++ static/javascript/chat.js
@@ -10,6 +10,7 @@ socket.on('connect', function() {
       e.preventDefault()
       let user_name = $( 'input#exampleInputEmail1' ).val()
       let user_id = $( '.mus' ).val()
+      console.log(user_id)
       socket.emit( 'my event', {
         user_name : user_name,
         userid : user_id
@@ -22,5 +23,5 @@ socket.on('connect', function() {
 
   socket.on( 'my response', function( msg ) {
     console.log( msg.user_name )
-      $( '.list-group' ).append( `<li class="list-group-item" style="background-color: ;">message from  ${ msg['userid']  } ${msg.user_name}</li>`)
+      $( '.list-group' ).append( `<li class="list-group-item" style="background-color: ;">message from  ${ msg.userid } ${msg.user_name}</li>`)
   })
