879bea91e4a47e9419b6ce018aedac620f997fd6
developython14 <djmel1955@gmail.com>
2022-07-24 17:50:48 +0100

    work on chat systeme namespaces

--- static/javascript/chat.js
+++ static/javascript/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://' + document.domain + ':' + location.port);
+var socket = io.connect("http://127.0.0.1:5000/chat/u/62dd5c7ceeb1a331b5e01bf2");
 
 
 socket.on('connect', function() {
